ae2893127646baaaf0d99e0714e8ca9c9f5c09bf
Sple <Shenb051102@outlook.com>
2026-03-08 21:03:14 +0800

    Add solution for Round 124 problem A

---
+++ WeeklyContext/Round124/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const int INF = 0x3f3f3f3f;
+const int N =  2e5 + 10;
+void solve(){
+    int x;
+    cin >> x;
+    if (x < 1600) {
+        cout << "Rated\n";
+    } else {
+        cout << "Unrated\n";
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    t = 1;
+    while (t--) solve();
+    return 0;
+}
