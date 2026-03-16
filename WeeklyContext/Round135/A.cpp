07ad40ff631d806ed4f9ee7751390388f469f9ff
Sple <Shenb051102@outlook.com>
2026-03-16 19:56:04 +0800

    A

---
+++ WeeklyContext/Round135/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const int INF = 1 << 30;
+const double eps = 1e-7;
+const int N =  2e5 + 10;
+void solve(){
+    int n;
+    cin >> n;
+    cout << n / 3 * 3 << "\n";
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    t = 1;
+    while (t--) solve();
+    return 0;
+}
