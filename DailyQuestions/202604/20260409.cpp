86e017a04831ff67948c4f15893c9896045a158f
Sple <Shenb051102@outlook.com>
2026-04-09 13:20:38 +0800

    DAG上的期望DP+拓扑/暴力DFS
    Refactor code structure and improve readability.

--- DailyQuestions/202604/20260409.cpp
+++ DailyQuestions/202604/20260409.cpp
@@ -1,4 +1,3 @@
-//或者期望DP+拓扑可以做到O(n+m)，以及全概率公式
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -49,3 +48,61 @@ int main(){
     while (t--) solve();
     return 0;
 }
+/*反向建边+期望DP+拓扑，正向的话是加权平均，都是O(n+m)
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+#define lowbit(x) (x) & (-x)
+#define vec vector<int>
+#define vvec vector<vector<int>>
+#define rep(i, a, b) for(int i = (a); i <= (int)(b); ++i)
+const int INF = 1 << 30;
+const ll LINF = 1LL << 60;
+const double eps = 1e-7;
+const int N =  2e5 + 10;
+struct Edge{
+    int v, w;
+};
+vector<Edge> adj[N];
+void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> in(n + 1, 0), siz(n + 1, 0);
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[v].push_back({u, w});
+        in[u]++;
+        siz[u]++;
+    }
+
+    vector<double> f(n + 1, 0.0); 
+    queue<int> q;
+    q.push(n);
+    f[n] = 0;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (auto [u, w] : adj[v]) {
+            f[u] += (f[v] + w) / siz[u];
+            in[u]--;
+            if (in[u] == 0) {
+                q.push(u);
+            }
+        }
+    }
+
+    cout << fixed << setprecision(2) << f[1] << "\n";
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}
+*/
