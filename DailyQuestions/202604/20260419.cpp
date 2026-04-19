862187ffd608490c9702a1e11aedf10c058cd87a
Sple <Shenb051102@outlook.com>
2026-04-19 22:19:57 +0800

    思维

---
+++ DailyQuestions/202604/20260419.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i += 2) {
+        cout << i << " ";
+    }
+    for (int i = 1; i <= n; i += 2) {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
