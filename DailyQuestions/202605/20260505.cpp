fd5eca6fa66feb735a433eb78505f7efdcfade90
Sple <Shenb051102@outlook.com>
2026-05-05 16:45:19 +0800

    模拟思维

---
+++ DailyQuestions/202605/20260505.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        if (n == 1 || n == 2) cout << "1\n";
+        else cout << "0\n";
+    }
+}
+// 64 位输出请用 printf("%lld")
