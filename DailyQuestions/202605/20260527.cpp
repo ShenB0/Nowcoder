2c54a42b88be7abf900168e8a5169023294671cf
Sple <Shenb051102@outlook.com>
2026-05-27 20:34:01 +0800

    思维

---
+++ DailyQuestions/202605/20260527.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    if (a != b) cout << "1\n";
+    else cout << a << "\n";
+}
+// 64 位输出请用 printf("%lld")
