96298b77d143460b0ed4a093f311d74663c86e1b
Sple <Shenb051102@outlook.com>
2026-06-08 13:03:14 +0800

    模拟

---
+++ WeeklyContext/Round147/B.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+
+#define debug(x) cout << #x << " = " << x << endl;
+#define lowbit(x) (x) & (-x)
+#define vec vector<int>
+#define vvec vector<vector<int>>
+#define rep(i, a, b) for(int i = (a); i <= (int)(b); ++i)
+
+const int INF = 1 << 30;
+const ll LLINF = 1LL << 60;
+const double eps = 1e-7;
+const int N =  2e5 + 10;
+
+void solve(){
+    int x;
+    cin >> x;
+    ll ans = 0;
+    ans += x / 10 * 7;
+    x %= 10;
+    ans += x / 5 * 4;
+    cout << ans << "\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}
