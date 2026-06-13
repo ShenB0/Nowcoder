0b27ea252e658cccd0befffb946b9be7c9477b5a
Sple <Shenb051102@outlook.com>
2026-06-13 14:15:38 +0800

    模拟

---
+++ DailyQuestions/202606/20260613.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+
+#define debug(x) cout << #x << " = " << x << endl;
+#define lowbit(x) (x) & (-x)
+#define ALL(x) (x).begin(), (x).end()
+
+const int INF = 1 << 30;
+const ll LLINF = 1LL << 60;
+const double eps = 1e-7;
+const int N =  2e5 + 10;
+
+void init(){
+    
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        mp[b[i]]++;
+    }
+
+    int ans = 0;
+    for (auto [v, c] : mp) {
+        if (c == 1) {
+            cout << "-1\n";
+            return ;
+        }
+        ans += (c + 1) / 2;
+    }
+
+    cout << ans << "\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    init();
+
+    int t = 1;
+    while (t--) solve();
+    
+    return 0;
+}
